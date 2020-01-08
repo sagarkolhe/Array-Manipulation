40b465f8a24a20415c09bcd9fea6c6718ab816de
sagar kolhe <sagarkolhe4242@gmail.com>
2020-01-08 13:28:47 +0530

    Add files via upload

---
+++ missingnum.py
@@ -0,0 +1,26 @@
+#the most easy solution is this 
+"""
+    miss = sum(range(1,n+1)) - sum(arr)
+    return miss
+"""
+
+
+def missing (arr,n):
+
+    tot = 0
+    for i in range(len(arr)):
+        tot = tot + arr[i]
+   
+
+    sum1 =0
+    for i in range (n+1):
+        sum1 = sum1 + i 
+   
+    miss = sum1 - tot
+
+    return miss
+    
+n =10
+arr = [1,2,3,4,6,7,8,9,10]
+print(missing(arr,n))
+
